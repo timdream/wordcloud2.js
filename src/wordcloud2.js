0f54ab7b36aa849a673d9bda73a6c821d327306b
Timothy Guan-tin Chien <timdream@gmail.com>
2019-11-23 12:20:49 -0800

    v1.1.1

--- src/wordcloud2.js
+++ src/wordcloud2.js
@@ -2,7 +2,7 @@
  * wordcloud2.js
  * http://timdream.org/wordcloud2.js/
  *
- * Copyright 2011 - 2013 Tim Chien
+ * Copyright 2011 - 2019 Tim Guan-tin Chien and contributors.
  * Released under the MIT license
  */
 
